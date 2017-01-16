9f9cc745c0ac4f3cbeec1289293e23d9347e3808
Katie Fritz <katie@katiemfritz.com>
2017-01-16 12:54:37 -0500

    not working

---
+++ responsive-iframes.php
@@ -0,0 +1,30 @@
+<?php
+/*
+Plugin Name: Responsive iframes
+Description: Applying NPR's responsive iframes code
+Version:     20170112
+Author:      Katie M Fritz
+Author URI:  http://katiemfritz.com
+License:     MIT
+*/
+
+defined( 'ABSPATH' ) or die( 'No script kiddies please!' );
+
+// Include the script from http://npr.github.io/responsiveiframe/
+function responsive_iframes_enqueue_script() {
+  wp_enqueue_script( 'iframes', 'https://cdn.rawgit.com/npr/responsiveiframe/master/dist/jquery.responsiveiframe.min.js', array ( 'jquery' ), 1.1);
+}
+add_action('wp_enqueue_scripts', 'responsive_iframes_enqueue_script');
+
+// If there's a problem retrieving the js file, use this as the source instead: plugin_dir_url( __FILE__ ) . 'jquery.responsiveiframe.js',
+
+add_action('wp_head','responsive_iframes');
+function responsive_iframes() { ?>
+  <script>
+    ;(function($){
+    $(function(){
+    $('.iframe').responsiveIframe({ xdomain: '*'});
+    });
+    })(jQuery);
+  </script>
+<?php }
