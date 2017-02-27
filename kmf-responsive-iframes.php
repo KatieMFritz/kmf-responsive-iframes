218ff7b1c32b0a9e22e1ff73d5e3318b2c29f29f
Katie Fritz <katie@katiemfritz.com>
2017-02-27 14:12:05 -0500

    update conditional logic

--- kmf-responsive-iframes.php
+++ kmf-responsive-iframes.php
@@ -12,23 +12,6 @@ License URI: https://opensource.org/licenses/MIT
 
 defined( 'ABSPATH' ) or die( 'Nope.' );
 
-// Add an admin page under "Plugins" to collect and display information
-add_action('admin_menu', 'settings_page_create');
-function settings_page_create() {
-  $page_title = 'KMF responsive iframes options';
-  $menu_title = 'iframes settings';
-  $capability = 'manage_options';
-  $menu_slug = 'kmf-responsive-iframes';
-  $function = 'options_display';
-  $icon_url = '';
-  $position = 99;
-  add_plugins_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
-}
-
-function options_display(){
-  include 'settings.php';
-}
-
 // Include base responsiveiframe.js script
 function responsive_iframes_enqueue_script() {
   wp_enqueue_script( 'iframes', plugin_dir_url( __FILE__ ) . 'jquery.responsiveiframe.js', array ( 'jquery' ), 1.1);
@@ -44,11 +27,49 @@ function responsive_iframes() { ?>
   </script>
 <?php }
 
+// Add an admin page under "Plugins" to collect and display information
+add_action('admin_menu', 'settings_page_create');
+function settings_page_create() {
+  $page_title = 'KMF responsive iframes options';
+  $menu_title = 'iframes settings';
+  $capability = 'manage_options';
+  $menu_slug = 'kmf-responsive-iframes';
+  $function = 'options_display';
+  $icon_url = '';
+  $position = 99;
+  add_plugins_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
+}
+
+function options_display(){
+  include 'instructions.php';
+  if (isset($_POST['where_to_use'])) {
+    update_option('where_to_use', $_POST['where_to_use']);
+    $where_to_use = $_POST['where_to_use'];
+  }
+  $where_to_use = get_option('where_to_use', 'default');
+  echo '<h2>Settings</h2>'.'<p>Currently applied to: '.$where_to_use.'</p>';
+  include 'options-form.php';
+}
+
 // Only load the scripts on certain pages, so you don't slow down other pages unnecessarily.
-// See https://developer.wordpress.org/reference/functions/is_page_template/
+// See https://developer.wordpress.org/reference/functions/is_page_template
 add_action('template_redirect', 'conditional_load');
 function conditional_load() {
-  if (!is_page()) { // change the conditional if needed!
+  if (isset($_POST['where_to_use'])) {
+    update_option('where_to_use', $_POST['where_to_use']);
+    $where_to_use = $_POST['where_to_use'];
+  }
+  $where_to_use = get_option('where_to_use', '');
+
+  if ($where_to_use === 'pages') {
+    $page_type = is_page();
+  } else if ($where_to_use === 'posts') {
+    $page_type = is_single();
+  } else {
+    $page_type = is_single() || is_page();
+  }
+
+  if ($page_type) { // change the conditional if needed!
     add_action('wp_enqueue_scripts', 'responsive_iframes_enqueue_script');
     add_action('wp_head','responsive_iframes');
   }
---
+++ options-form.php
@@ -0,0 +1,16 @@
+<form method="POST">
+  <fieldset>
+    <label for="where_to_use">Where to use?</label>
+    <select id="where_to_use" name="where_to_use">
+      <option selected disabled>Select</option>
+      <option value="pages">All pages</option>
+      <option value="posts">All posts</option>
+      <option value="pages-and-posts">All pages AND posts</option>
+    </select>
+    <br>
+    <!--<label for="page_template">Template Name</label>
+    <input type="text" name="page_template" id="where_to_use" value="">
+    <br>-->
+    <input type="submit" value="Save" class="button button-primary button-large">
+  </fieldset>
+</form>
