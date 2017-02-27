e416f41e8eaa616e613bf5bf93aeb69ebe93dbe6
Katie Fritz <katie@katiemfritz.com>
2017-02-27 13:05:41 -0500

    improve settings page - still only does instructions

--- kmf-responsive-iframes.php
+++ kmf-responsive-iframes.php
@@ -12,37 +12,21 @@ License URI: https://opensource.org/licenses/MIT
 
 defined( 'ABSPATH' ) or die( 'Nope.' );
 
-// Add an admin menu to collect and display information
-add_action('admin_menu', 'plugin_menu');
-
-function plugin_menu(){
-  add_menu_page( 'Responsive iframes Page', 'iframes', 'manage_options', 'kmf-responsive-iframes', 'options_init' );
+// Add an admin page under "Plugins" to collect and display information
+add_action('admin_menu', 'settings_page_create');
+function settings_page_create() {
+  $page_title = 'KMF responsive iframes options';
+  $menu_title = 'iframes settings';
+  $capability = 'manage_options';
+  $menu_slug = 'kmf-responsive-iframes';
+  $function = 'options_display';
+  $icon_url = '';
+  $position = 99;
+  add_plugins_page( $page_title, $menu_title, $capability, $menu_slug, $function, $icon_url, $position );
 }
 
-function options_init(){
-  ?>
-    <h1>KMF Responsive iFrames</h1>
-    <p><a href="https://github.com/KatieMFritz/kmf-responsive-iframes" target="_blank">Current version and documentation</a></p>
-    <section>
-      <h2>How to set up the "child" site</h2>
-      <p>The child site is the site that will be displayed <em>inside</em> your iframes.</p>
-      <ol>
-        <li>Upload <a href="https://github.com/KatieMFritz/kmf-responsive-iframes/blob/master/iframes-child.js" target="_blank"><code>iframes-child.js</code></a> to a public directory in your <em>child</em> website.</li>
-        <li>Copy the public file path or link.</li>
-        <li>Add the following code to your child website (in the header, footer, or a "Global JavaScript" section):
-          <pre><code>&lt;script src="[file path you copied for iframes-child.js]"&gt;&lt;/script&gt;</code></pre></li>
-      </ol>
-    </section>
-    <section>
-      <h2>How to add an iframe to a page or post</h2>
-      <p>Once you've installed everything:</p>
-      <ol>
-        <li>Copy the URL for the page you want to embed (the child page).</li>
-        <li>In your WordPress page or post, go to the text editor (<em>not</em>the visual editor) and paste the following code:
-          <pre><code>&lt;iframe class="iframe" src="[URL OF CHILD PAGE]"&gt;&lt;/iframe&gt;</code></pre></li>
-      </ol>
-    </section>
-  <?php
+function options_display(){
+  include 'settings.php';
 }
 
 // Include base responsiveiframe.js script
